cb5ab2bf742b6b7fa1a549bbd28665a04e3b3658
Sedrick <digitalphenom0@gmail.com>
2021-06-26 23:17:12 -0700

    refactor color picker action

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 
+
 // Etch-a-Sketch
 const screen = document.querySelector('.screen');
 
@@ -6,6 +7,7 @@ let userInput =16;
 
 let div = ""
 let div2 = ""
+
 
 const gridX = (userInput) => {
     
@@ -17,18 +19,31 @@ const gridX = (userInput) => {
         div = document.createElement('div');
         div.classList.add('box1');
         screen.append(div);
-    }
-    let box1 = document.querySelectorAll('.box1');
-    
-    box1.forEach(box => {
-        
-        const mouseOver = () => {
-            box.addEventListener('mouseover', e => {
-                e.target.classList.add('box2')
-            });
+        function Click(e) {
+            e.target.style.backgroundColor = div2;
+           
+      }
+    //   mouse held down
+      function ClickHover(e) {
+        if (e.buttons > 0) {
+              e.target.style.backgroundColor = '#023047';
+            
         }
-        mouseOver();
-    });
+    
+      }
+      function listen() {
+        grids = document.querySelectorAll('.box1');
+        for (let i = 0; i < grids.length; i++) {
+          grids[i].addEventListener('mousedown', Click);
+          // listen for a mouse over if mouse button is pressed
+          grids[i].addEventListener('mouseenter', ClickHover);
+        }
+    
+      }
+      
+      listen();
+    }
+
     const resetBtn = () => {
 
         const resetBtn = document.querySelector('#reset');
@@ -38,39 +53,59 @@ const gridX = (userInput) => {
     }
     resetBtn()
 }
-gridX(userInput);
-
-
-const colorBtn = document.querySelector('#colorBtn')
-    colorBtn.addEventListener('click', () => {
-        
-        let box1 = document.querySelectorAll('.box1');
-    
+  
         function randomColor(){
         const colors = [
-            'red',
-            'orange',
-            'yellow',
-            'green',
-            'blue',
-            'indigo',
-            'purple',
-            'violet'
+            '#4cc9f0',
+            '#4895ef',
+            '#4361ee',
+            '#3f37c9',
+            '#3a0ca3',
+            '#480ca8',
+            '#560bad',
+            '#7209b7',
+            '#b5179e',
+            '#f72585'
+            
         ]
         let randomColor = Math.floor(Math.random() * colors.length)
         let colorPicker = colors[randomColor]
         return colorPicker;
     }
-    
-    box1.forEach(box => {    
-        const colorBtn = () => {
-            box.addEventListener('mouseover', e => {
-                box.style.backgroundColor = randomColor()
-                // console.log(randomColor())
-            // box.style.backgroundColor = "colorPicker"
-            })
-        }
-        colorBtn();
-     });
 
-    })
+const colorBtn = document.querySelector('#colorBtn')
+
+    colorBtn.addEventListener('click', () => {
+
+    for ( i = 0; i < userInput **2; i++) {
+        
+    colorDiv = document.createElement('div');
+    colorDiv.classList.add('box1');
+    //mouse click
+    function Click(e) {
+        e.target.style.backgroundColor = randomColor();
+  }
+  // mouse held down
+  function ClickHover(e) {
+    if (e.buttons > 0) {
+          e.target.style.backgroundColor = randomColor();
+    }
+
+  }
+  function listen() {
+    grids = document.querySelectorAll('.box1');
+    for (let i = 0; i < grids.length; i++) {
+      grids[i].addEventListener('mousedown', Click);
+      // listen for a mouse over and change colour only if mouse button is pressed
+      grids[i].addEventListener('mouseenter', ClickHover);
+    }
+  
+  }
+
+  
+}
+listen();
+});
+gridX(userInput);
+
+
