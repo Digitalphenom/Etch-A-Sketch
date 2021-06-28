7053af5923eac38ad600b6e4246215d871051396
Sedrick <digitalphenom0@gmail.com>
2021-06-27 23:50:24 -0700

    Add dynamic grid upon user input

--- script.js
+++ script.js
@@ -2,110 +2,113 @@
 
 // Etch-a-Sketch
 const screen = document.querySelector('.screen');
-
-let userInput =16;
-
+const screenSize = 584;
+let userInput = prompt ('enter your grid size',);
+let gridDimension = (screenSize/userInput) -2;
 let div = ""
-let div2 = ""
-
 
 const gridX = (userInput) => {
     
     screen.style.gridTemplateColumns = `repeat(${userInput}, auto)`;
-    screen.style.gridTemplaterows = `repeat(${userInput}, auto)`;
+    screen.style.gridTemplateRows = `repeat(${userInput}, auto)`;
     
     for ( i = 0; i < userInput **2; i++) {
         
         div = document.createElement('div');
         div.classList.add('box1');
         screen.append(div);
+        div.style.width = `${gridDimension}px`;
+        div.style.height = `${gridDimension}px`;
+        
         function Click(e) {
-            e.target.style.backgroundColor = div2;
-           
-      }
-    //   mouse held down
-      function ClickHover(e) {
-        if (e.buttons > 0) {
-              e.target.style.backgroundColor = '#023047';
+            e.target.style.backgroundColor = '#023047';
             
         }
-    
-      }
-      function listen() {
+        //   Hold mouse down
+        function ClickHover(e) {
+            if (e.buttons > 0) {
+                e.target.style.backgroundColor = '#023047';
+                
+        }
+        
+    }
+    function listen() {
         grids = document.querySelectorAll('.box1');
         for (let i = 0; i < grids.length; i++) {
           grids[i].addEventListener('mousedown', Click);
-          // listen for a mouse over if mouse button is pressed
+          // listen for mouse over if mouse button press
           grids[i].addEventListener('mouseenter', ClickHover);
         }
+        
+    }
     
-      }
-      
-      listen();
-    }
-
-    const resetBtn = () => {
-
-        const resetBtn = document.querySelector('#reset');
-        resetBtn.addEventListener('click', function(){
-            location.reload();
-        })
-    }
-    resetBtn()
+    listen();
 }
-  
-        function randomColor(){
-        const colors = [
-            '#4cc9f0',
-            '#4895ef',
-            '#4361ee',
-            '#3f37c9',
-            '#3a0ca3',
-            '#480ca8',
-            '#560bad',
-            '#7209b7',
-            '#b5179e',
-            '#f72585'
+
+}
+
+
+function randomColor(){
+    const colors = [
+        '#4cc9f0',
+        '#4895ef',
+        '#4361ee',
+        '#3f37c9',
+        '#3a0ca3',
+        '#480ca8',
+        '#560bad',
+        '#7209b7',
+        '#b5179e',
+        '#f72585'
             
         ]
         let randomColor = Math.floor(Math.random() * colors.length)
         let colorPicker = colors[randomColor]
         return colorPicker;
     }
-
-const colorBtn = document.querySelector('#colorBtn')
-
+    
+    const colorBtn = document.querySelector('#colorBtn')
+    
     colorBtn.addEventListener('click', () => {
-
-    for ( i = 0; i < userInput **2; i++) {
         
-    colorDiv = document.createElement('div');
-    colorDiv.classList.add('box1');
-    //mouse click
-    function Click(e) {
-        e.target.style.backgroundColor = randomColor();
-  }
-  // mouse held down
-  function ClickHover(e) {
-    if (e.buttons > 0) {
-          e.target.style.backgroundColor = randomColor();
+        for ( i = 0; i < userInput **2; i++) {
+            
+            colorDiv = document.createElement('div');
+            colorDiv.classList.add('box1');
+            //mouse click
+            function Click(e) {
+                e.target.style.backgroundColor = randomColor();
+            }
+            // mouse hold down
+            function ClickHover(e) {
+                if (e.buttons > 0) {
+                    e.target.style.backgroundColor = randomColor();
+                }
+                
+            }
+            function listen() {
+                grids = document.querySelectorAll('.box1');
+                for (let i = 0; i < grids.length; i++) {
+                    grids[i].addEventListener('mousedown', Click);
+                    // listen for mouse over change color if mouse button press
+                    grids[i].addEventListener('mouseenter', ClickHover);
+                }
+                
+            }
+            
+            
+        }
+        listen();
+    });
+    const resetBtn = () => {
+        
+        const resetBtn = document.querySelector('#reset');
+        resetBtn.addEventListener('click', function(){
+            location.reload();
+        })
     }
-
-  }
-  function listen() {
-    grids = document.querySelectorAll('.box1');
-    for (let i = 0; i < grids.length; i++) {
-      grids[i].addEventListener('mousedown', Click);
-      // listen for a mouse over and change colour only if mouse button is pressed
-      grids[i].addEventListener('mouseenter', ClickHover);
-    }
-  
-  }
-
-  
-}
-listen();
-});
-gridX(userInput);
-
-
+    resetBtn()
+    gridX(userInput);
+    
+    
+    
